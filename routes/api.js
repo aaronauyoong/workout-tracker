3ec520614c88d08b356281307de475f02c0999b0
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-07-04 17:00:45 +0800

    added pseudo code for api.js in routes folder. Code includes getting all workouts, creating new workout.

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,30 @@
 const router = require("express").Router();
+const { Workout } = require("../models");
+
+// get all workouts
+router.get("/api/workouts", (req, res) => {
+	Workout.find({})
+		.sort({ name: 1 })
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+// create a new workout
+router.post("/api/workouts", (req, res) => {
+	Workout.create(body)
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+// edit an existing workout
+router.put();
 
 module.exports = router;
