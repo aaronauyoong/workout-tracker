af8d8cfa358152af3aa0d4ac6442bcc4fed352cc
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-07-05 17:43:37 +0800

    updated unused parameters (e.g. req) to '_'. Added async await into all api calls. added function to calculate total distance. refactored all code, used Prettier to format code

--- routes/api.js
+++ routes/api.js
@@ -1,71 +1,82 @@
 const router = require("express").Router();
 const { Workout } = require("../models/workout.js");
 
-function arraySum(array) {
-	let result = 0;
+function arraySumDuration(array) {
+	let sum = 0;
 	for (let index = 0; index < array.length; index++) {
-		result += array[index];
+		sum += array[index].duration;
 	}
-	return result;
+	return sum;
+}
+
+function arraySumDist(array) {
+	let sum = 0;
+	for (let index = 0; index < array.length; index++) {
+		sum += array[index].distance;
+	}
+	return sum;
 }
 
 // get all workouts
-router.get("/api/workouts", (req, res) => {
-	Workout.find({})
-		.sort({ name: 1 })
-		.then((dbWorkouts) => {
-			const workouts = dbWorkouts.map((workout) => {
-				// calc the total duration for each workout
-				const durations = workout.exercises.map(
-					(exercise) => exercise.duration
-				);
+router.get("/api/workouts", async (_, res) => {
+	try {
+		const allWorkouts = await Workout.find({})
+			.sort({ name: 1 })
+			.then((allWorkouts) => {
+				const workouts = allWorkouts.map((workout) => {
+					// calc the total duration for each workout
+					const totalDuration = arraySumDuration(workout.exercises);
+					const totalDistance = arraySumDist(workout.exercises);
 
-				const totalDuration = arraySum(durations);
+					return {
+						...workout.toObject(),
+						totalDuration,
+						totalDistance,
+					};
+				});
 
-				return {
-					...workout.toObject(),
-					totalDuration,
-				};
+				res.json(workouts);
 			});
-
-			res.json(workouts);
-		})
-		.catch((err) => {
-			res.status(400).json(err);
-		});
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
 // create a new workout
-router.post("/api/workouts", ({ body }, res) => {
-	Workout.create(body)
-		.then((dbWorkout) => {
-			res.json(dbWorkout);
-		})
-		.catch((err) => {
-			res.status(400).json(err);
+router.post("/api/workouts", async ({ body }, res) => {
+	console.log(body);
+	try {
+		const addWorkout = await Workout.create(body).then((addWorkout) => {
+			res.json(addWorkout);
 		});
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
 // edit an existing workout by ID
-router.put("/api/workouts/:id", (req, res) => {
-	Workout.findByIdAndUpdate({ _id: req.params.id }, { exercises: req.body })
-		.then((dbWorkout) => {
-			res.json(dbWorkout);
-		})
-		.catch((err) => {
-			res.status(400).json(err);
+router.put("/api/workouts/:id", async (req, res) => {
+	try {
+		const editWorkout = await Workout.findByIdAndUpdate(
+			{ _id: req.params.id },
+			{ exercises: req.body }
+		).then((editWorkout) => {
+			res.json(editWorkout);
 		});
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
 // get workouts/range for the stats page - links to frontend function getWorkoutsInRange()
-router.get("/api/workouts/range", ({}, res) => {
-	Workout.find({})
-		.then((dbWorkout) => {
-			res.json(dbWorkout);
-		})
-		.catch((err) => {
-			res.status(400).json(err);
+router.get("/api/workouts/range", async (_, res) => {
+	try {
+		const workoutStats = await Workout.find({}).then((workoutStats) => {
+			res.json(workoutStats);
 		});
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
 module.exports = router;
