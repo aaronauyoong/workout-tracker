6c0d2e80c314df5ce89a8115c1d2e374cba5f8c7
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-07-05 13:01:11 +0800

    finalised htmlRoutes.js to use router middleware to load html pages properly

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,12 @@
 const path = require("path");
+const router = require("express").Router();
 
-module.exports = (app) => {
-	app.get("/exercise", (req, res) => {
-        console.log("getting exercise path")
-		res.sendFile(path.join(__dirname, "../public/exercise.html"));
-	});
+router.get("/exercise", (req, res) => {
+	res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
-	app.get("/stats", (req, res) => {
-        console.log("getting stats path")
-		res.sendFile(path.join(__dirname, "../public/stats.html"));
-	});
-};
+router.get("/stats", (req, res) => {
+	res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
